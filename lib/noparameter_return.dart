42ace99685e7c84ec882376e30edd585f8336554
Kantita01 <kantita5069@gmail.com>
2023-09-01 09:31:37 +0700

    first commit

---
+++ lib/noparameter_return.dart
@@ -0,0 +1,10 @@
+//3.function with no parameter and return type
+void primeMinistrator(){
+  String message = returnMinistrator();
+  print(message);
+  // print(returnMinistrator());
+}
+
+String returnMinistrator(){
+  return ("Mr.Jeff Bezos");
+}
---
+++ lib/parameter_no_return.dart
@@ -0,0 +1,10 @@
+//2.function with parameter and no return type
+void sendName(){
+  String name = "Mr.Elon Musk";
+  printName(name); //Called printName function
+}
+
+void printName(String reName){
+  print(reName);
+}
+
